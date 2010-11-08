9607a839c63423cde6500bfe4b2ef743769479a0
textform <>
2010-11-08 10:37:06 +0000

    - fixed upgrade log

--- sql/postgresql/upgrade/upgrade-3.4.1.0.8-3.5.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.4.1.0.8-3.5.0.0.0.sql
@@ -1,6 +1,6 @@
 -- upgrade-3.4.1.0.8-3.5.0.0.0.sql
 
-SELECT acs_log__debug('/packages/intranet-core/sql/postgresql/upgrade/upgrade-3.4.1.0.8-3.5.0.0.0.sql','');
+SELECT acs_log__debug('/packages/intranet-rest/sql/postgresql/upgrade/upgrade-3.4.1.0.8-3.5.0.0.0.sql','');
 
 
 
